6f9e809d5254b35db4e9662a4946d07d504aadc7
Morgan Hoarau <morgansb.hoarau@gmail.com>
2021-06-11 22:46:14 +0100

    3C scene with input system,

---
+++ Assets/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour
+{
+    private Vector2 movementAction = Vector2.zero;
+    private bool featureAction = false;
+
+    public void MovementActionCallback(InputAction.CallbackContext context)
+    {
+        this.movementAction = context.ReadValue<Vector2>();
+    }
+
+    public void JumpActionCallback(InputAction.CallbackContext context)
+    {
+        this.featureAction = context.ReadValue<int>() == 0;
+
+        // if (featureAction)
+        // {
+        // start feature...
+        // }
+        // else
+        // {
+        // stop feature...
+        // }
+    }
+}
