282d633a486c1f7fea96e653bb4d5dc05c4b2e48
Maxence Beaumont <m.beaumont@ohbibi.com>
2021-06-11 22:38:55 +0200

    [Hierarchy] Adding some files to work on the game

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Anchor : MonoBehaviour
+public class NetworkManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
