374edfecd3b19b216d1f012a8bc418bf3352da3d
Maxence Beaumont <m.beaumont@ohbibi.com>
2021-06-12 19:27:05 +0200

    [Utils] AnimatedGameplayBrick component

---
+++ Assets/Scripts/Utils/AnimatedGameplayBrick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedGameplayBrick : MonoBehaviour
+{
+	[Header("Following this formula: initialPosition + transform.up * displacementCurve.Evaluate(time / frequency) * intensity")]
+    [SerializeField] private AnimationCurve displacementCurve = new AnimationCurve();
+	[SerializeField] float intensity = 10;
+	[SerializeField] float frequency = 0.5f;
+
+
+	private Vector3 initialpos;
+
+	private void Awake()
+	{
+		initialpos = transform.position;
+	}
+
+	void Update()
+    {
+        transform.position = initialpos + transform.up * displacementCurve.Evaluate(Time.time / frequency) * intensity;
+    }
+}
