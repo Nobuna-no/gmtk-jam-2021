f7e86ef9243d10c026c2f9bd11b6e09212c49dd6
Maxence Beaumont <m.beaumont@ohbibi.com>
2021-06-12 16:54:01 +0200

    [Controller] Main Menu et controller

---
+++ Assets/Scripts/UI/MenuHandling.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuHandling : MonoBehaviour
+{
+
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject multiplayerControlSelection;
+    [SerializeField] private string levelToLoad = "2-Gameplay";
+    [SerializeField] private ToggleSwitch multiP1Control;
+    [SerializeField] private ToggleSwitch multiP2Control;
+    [SerializeField] private ToggleSwitch soloP1Control;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainMenu.SetActive(true);
+        multiplayerControlSelection.SetActive(false);
+    }
+
+    public void OpenMultiplayerControls()
+	{
+        mainMenu.SetActive(false);
+        multiplayerControlSelection.SetActive(true);
+    }
+
+    public void OpenSoloControls()
+    {
+        Play(0);
+    }
+
+    public void Play(int gameMode)
+	{
+        if (gameMode == 0)
+            PlayerPrefs.SetInt("P1Controls", 0);
+        else
+		{
+            PlayerPrefs.SetInt("P1Controls", multiP1Control.IsA ? 1 : 2);
+            PlayerPrefs.SetInt("P2Controls", multiP2Control.IsA ? 0 : 1);
+        }
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(levelToLoad);
+	}
+}
---
+++ Assets/Scripts/UI/ToggleSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSwitch : MonoBehaviour
+{
+    [SerializeField] private Toggle a;
+    [SerializeField] private Toggle b;
+
+    public bool IsA { get { return a; } }
+    public bool IsB { get { return b; } }
+
+	private void Awake()
+	{
+		a?.onValueChanged.AddListener(delegate
+		{
+			OnValueChange("a");
+		});
+
+		b?.onValueChanged.AddListener(delegate
+		{
+			OnValueChange("b");
+		});
+	}
+
+	private void OnValueChange(string toogleName)
+	{
+		switch(toogleName)
+		{
+			case "a":
+				b.isOn = !a.isOn;
+				break;
+			case "b":
+				a.isOn = !b.isOn;
+				break;
+		}
+
+	}
+}
