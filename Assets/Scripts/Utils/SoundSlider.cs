a56f79133abc306f32f10f868b5ef03d2dbd41ae
Maxence Beaumont <m.beaumont@ohbibi.com>
2021-06-13 19:47:57 +0200

    [Menu] MainMenu

---
+++ Assets/Scripts/Utils/SoundSlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class SoundSlider : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private string floatName;
+
+    private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        float f;
+		mixer.GetFloat(Mathf.exfloatName, out f);
+        slider.value = f;
+    }
+
+    public void OnValueChanged()
+	{
+        mixer.SetFloat(floatName, Mathf.Log10(slider.value) *20);
+	}
+}
