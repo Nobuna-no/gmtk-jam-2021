3952249892cc21324a882b548ad813d182218989
jmekhaemar <j.mekhaemar@gmail.com>
2021-06-13 12:44:44 +0100

    Added rope

---
+++ Assets/Scripts/Graphics/Rope.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+	public Rigidbody2D hook;
+	public HingeJoint2D end;
+	public RopeSegment ropeSegmentPrefab;
+	public int numLinks = 5;
+	
+    void Start()
+	{
+		RopeSegment prevSegment = null;
+		for (int i = 0; i < numLinks; ++i)
+		{
+			RopeSegment newSegment = Instantiate(ropeSegmentPrefab);
+			newSegment.transform.parent = transform;
+			newSegment.transform.position = transform.position;
+
+			if (prevSegment)
+				newSegment.SetConnection(prevSegment);
+			else
+				newSegment.SetHookConnection(hook);
+
+			prevSegment = newSegment;
+		}
+
+		if (prevSegment)
+		{
+			end.connectedBody = prevSegment.Rigidbody;
+			end.connectedAnchor = new Vector2(0.0f, -prevSegment.Height);
+		}
+	}
+}
